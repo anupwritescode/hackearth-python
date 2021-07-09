1eef80b9438059b5a6a53ec6a6f4538951174451
anups1 <anupsalve8@gmail.com>
2021-07-10 00:50:52 +0530

    update day 6

--- holiday-season/holiday-season.py
+++ holiday-season/holiday-season.py
@@ -1,12 +1,28 @@
 len_ = int(input())
 string_ = input()
 total_count = 0
-already_counted = []
-for character in range(0, len_) :
-    sub_string = string_[character]
-    for next_c in range(character + 1, len_) :        
-        sub_string += string_[next_c]
-        if sub_string not in already_counted :
-            total_count += (string_.count(sub_string) - 1)
-            already_counted.append(sub_string)
+alphabet_map = {}
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+for character_pos in range(len_) :
+    character = string_[character_pos]
+    if character not in alphabet_map :
+        alphabet_map[character] = [0] * len_
+    alphabet_map[character][character_pos] = 1
+
+for letter in alphabet :
+    character = string_[character_pos]
+    for character_pos in range(1, len_) :
+        if letter in alphabet_map :
+            alphabet_map[letter][character_pos] += alphabet_map[letter][character_pos - 1]
+
+for character_pos in range(len_) :
+    character = string_[character_pos]
+    for c_pos_onwards in range((character_pos + 1), len_) :
+        c_next = string_[c_pos_onwards]
+        if character == c_next :
+            for letter in alphabet :
+                if letter in alphabet_map :
+                    total_count += ((alphabet_map[letter][len_-1] - alphabet_map[letter][c_pos_onwards]) * (alphabet_map[letter][c_pos_onwards - 1] - alphabet_map[letter][character_pos]))
+
 print(total_count)
